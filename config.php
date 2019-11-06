50a21de3ecc67e1a2f1b6b1e60ee04b8b68023b0
kamikazeone <44550472+kamikazeone@users.noreply.github.com>
2019-11-06 11:24:12 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$refresh_token = "AEu4IL3BryKT4WsbxXY_xbB86I3myrzHj4civkQMF5SGpdObMLKneJHylWwo3p-Eheo57xuSZ9qcWSjfFKDw7hbx2ERSGER17I02t4h4EZdgrkRC2uibXQVNc5gxLsIDEq22A9buUtVPeO9QbLIin_hzNx7NfrjFG-gZNDxFxqCxZ33gRRqc9-2PIFy8XYBNohA6JYa-eH8K8PPP9i0xRXIC435yKtVW_OL2cGRSdKvE6qgMMSTY-UziViB8A5BU3cfc_mjKdS5-tifBLW9c1GoAQh5iJq0EucoOWXFyfPW_7dAQUDvc_ru1aJfxVkDD95U4qgnPbWuo2bzV9RfSZxMMk4ykg9ThwQQppfpO4EMa6RLLOpM-8Uv6h7WdtoEvSlhTpin9zojgTON1FqQLnCwxEwkz-70qmRBMYk6XbXh5QR3cgNZ1u-kYfS6X-OVdj9cjibB06xHhlMdBT8YvVCpbI4hn4Efjrg";
+
+$email_coinbase = "bujuramteam123@gmail.com";
+
+$target_withdraw = 200000;
+
+$version = "13";
